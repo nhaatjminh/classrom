64a03923c1de9bf59df4ecba49ac0fa891f1a60f
nhaatjminh <minhphan.240199@gmail.com>
2021-11-16 23:51:47 +0700

    invite to class

--- src/Component/DetailClass/index.js
+++ src/Component/DetailClass/index.js
@@ -4,6 +4,13 @@ import { NavLink, useParams} from "react-router-dom"
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Navbar, Modal } from "react-bootstrap"
 import './index.css'
+import SendIcon from '@mui/icons-material/Send';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+
+
 const DetailClass = () => {
     const [data, setData] = useState({
             creator: "",
@@ -16,6 +23,9 @@ const DetailClass = () => {
     const [loadFirst, setLoadFirst] = useState(true);
     const [inviteLinkStudent, setInviteLinkStudent] = useState();
     const [inviteLinkTeacher, setInviteLinkTeacher] = useState();
+    const [showDialog, setShowDialog] = useState(false);
+    const [email, setEmail] = useState("");
+    const [roleInvite, setRoleInvite] = useState("Student");
     let params = useParams();
     const getDetail = async (id) => {
         var myHeaders = new Headers();
@@ -42,8 +52,6 @@ const DetailClass = () => {
         });
     }
 
-    const
-
     const getInviteLink = async (id,role) => { //truyen role vo day nhe bro
         var myHeaders = new Headers();
         myHeaders.append("Authorization", "Bearer " + localStorage.getItem("token"));
@@ -68,6 +76,72 @@ const DetailClass = () => {
             console.log('error', error);
         });
     }
+
+    const onHandleShow = () => {
+        setShowDialog(true);
+    }
+    const onHandleClose = () => {
+        setShowDialog(false);
+    }
+    const onChangeHandler = (e) => {
+        setEmail(e.target.value);
+    }
+    const handleChange = (e) => {
+        setRoleInvite(e.target.value);
+    }
+
+    const sendEmail = async (recipient, inviteLink, role) => {
+        var myHeaders = new Headers();
+        myHeaders.append("Authorization", "Bearer " + localStorage.getItem("token"));
+        myHeaders.append("Content-Type", "application/json");
+
+        var raw = JSON.stringify({
+            "recipient": recipient,
+            "inviteLink": inviteLink,
+            "role": role
+        });
+
+        var requestOptions = {
+            method: 'POST',
+            headers: myHeaders,
+            body: raw,
+            redirect: 'follow'
+        };
+
+        await fetch("http://localhost:5000/sendEmail", requestOptions)
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            }
+
+            throw Error(response.status);
+        })
+        .then(result => {
+            alert("Invitation was sent!");
+            onHandleClose();
+        })
+        .catch(error => {
+            alert("Fail to send invitation!");
+            onHandleClose();
+        });
+    }
+
+    
+
+    const onSubmitHandler = async (e) => {
+        e.preventDefault();
+
+        let link  = "";
+        if (roleInvite == "student") {
+            link = inviteLinkStudent
+        }
+        else {
+            link = inviteLinkTeacher
+        }
+
+        await sendEmail(email, link, roleInvite);
+    }
+
     if (loadFirst) {
         getInviteLink(params.id, 'student');
         getInviteLink(params.id, 'teacher');
@@ -91,6 +165,9 @@ const DetailClass = () => {
                     </NavLink>
                     </Navbar.Collapse>
                 </Navbar>
+                <div className="btn-new" style={{ margin: '10px 0'}}>
+                        <button className="btn btn-success" onClick={onHandleShow}> Invite </button>
+                </div>
 
                 <div className="container-fluid mt-5">
                     <h1 className="text-center">
@@ -108,16 +185,7 @@ const DetailClass = () => {
                     <div className="mt-3">
                         Description: {data.description}
                     </div>
-                    <div className="mt-3">
-                        <div className="col-12">
-                            Link Invite Student: {inviteLinkStudent}
-                        </div>
-                    </div>
-                    <div className="mt-3">
-                        <div className="col-12">
-                            Link Invite Teacher: {inviteLinkTeacher}
-                        </div>
-                    </div>
+                    
                     <div className="row">
                         <div className="col-5">
                             A
@@ -129,27 +197,48 @@ const DetailClass = () => {
                 </div>
 
                 <div>
-                    <Modal show={this.state.show} onHide={this.onHandleClose}>
+                    <Modal show={showDialog} onHide={onHandleClose}>
                         <Modal.Header closeButton>
-                        <Modal.Title>Adding Classroom</Modal.Title>
+                        <Modal.Title>Invitation</Modal.Title>
                         </Modal.Header>
                         <Modal.Body>
-                            <form onSubmit={this.onSubmitHandler} action="https://best-classroom-ever-api.herokuapp.com/classes" method="POST">
+                            <form onSubmit={onSubmitHandler}>
+                                <div className="mt-3" fullWidth>
+                                    <div className="col-12">
+                                        Link Invite Student: {inviteLinkStudent}
+                                    </div>
+                                </div>
+                                <div className="mt-3" fullWidth>
+                                    <div className="col-12">
+                                        Link Invite Teacher: {inviteLinkTeacher}
+                                    </div>
+                                </div>
+                                <div>
+                                    <FormControl style={{width: 120, margin: '10px 0'}}>
+                                        <InputLabel id="demo-simple-select-label">Role</InputLabel>
+                                        <Select
+                                            labelId="demo-simple-select-label"
+                                            id="demo-simple-select"
+                                            value={roleInvite}
+                                            label="Role"
+                                            onChange={handleChange}
+                                        >
+                                            <MenuItem value={"student"}>Student</MenuItem>
+                                            <MenuItem value={"teacher"}>Teacher</MenuItem>
+                                        </Select>
+                                        </FormControl>
+                                </div>
                                 <div className="row">
-                                    <div className="col-9">
-                                        <input type="text" name="name" className="form-control" placeholder="New class name..." onChange={this.onChangeHandler} />
+                                    <div className="col-10">
+                                        <input type="text" name="name" className="form-control" placeholder="Send this invitaion to email..." onChange={onChangeHandler} />
                                     </div>
                                     <div className="col">
-                                        <button type="submit" className="btn btn-success"> Add Class </button>
+                                        <button type="submit" className="btn btn-success"> <SendIcon/> </button>
                                     </div>
                                 </div>
                             </form>
                         </Modal.Body>
-                        <Modal.Footer>
-                        <div onClick={this.onHandleClose}>
-                            <button className="btn btn-dark" onClick={this.onHandleShow}> Close </button>
-                        </div>
-                        </Modal.Footer>
+                        
                     </Modal>
                 </div>
             </div>
