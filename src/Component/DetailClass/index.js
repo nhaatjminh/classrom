4735dcde95dec95f57222cfaa2023ba805828a1c
tdp2k <phuoc.tran@globee.hk>
2021-11-16 15:37:02 +0700

    change link fetch at detail page

--- src/Component/DetailClass/index.js
+++ src/Component/DetailClass/index.js
@@ -50,8 +50,8 @@ const DetailClass = () => {
             headers: myHeaders
         };
 
-        // await fetch("https://best-classroom-ever-api.herokuapp.com/classes/invitelink/" + id + "/" + role, requestOptions)
-        await fetch("http://localhost:5000/classes/invitelink/" + id + "/" + role, requestOptions)
+        await fetch("https://best-classroom-ever-api.herokuapp.com/classes/invitelink/" + id + "/" + role, requestOptions)
+        // await fetch("http://localhost:5000/classes/invitelink/" + id + "/" + role, requestOptions)
         .then(response => response.json())
         .then(result => {
             console.log("into")
